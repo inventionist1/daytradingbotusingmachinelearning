d572743c267743a69d8bd4145e8d7da5da79fd75
inventionist1 <42523031+inventionist1@users.noreply.github.com>
2025-01-16 07:50:47 -0600

    added more to load_data.py, uses json to store api keys

--- load_data.py
+++ load_data.py
@@ -1,17 +1,33 @@
 import requests
+import json
+from alpaca.trading.stream import TradingStream
 
+API_FILE:str = "C:\\Users\\Isaac\\Desktop\\trading bot\\daytradingbotusingmachinelearning\\apikey.json"
 
-def getAPIKey(filename:str) -> str:
-    try:
-        file = open(filename, "r")
-        data = file.read()
-        file.close()
+def get_API_keys(filename:str) -> str:
+    with open(filename, 'r') as file:
+        data = json.load(file)
         return data
-    except:
-        print("an error occured, please check the filename and try again");
-        return ""
+    return ''
+
+def connect(is_paper:bool=True):
+    keys = get_API_keys(API_FILE)
+    if not(is_paper):
+        alpaca_keys:list[str] = {
+            "TRADE_API_KEY":keys['real_key'],
+            "TRADE_API_SECRET":keys['real_secret'],
+            "TRADE_API_ENDPOINT":keys['real_endpoint']
+        }
+    else:
+        alpaca_keys:list[str] = {
+            "TRADE_API_KEY":keys['paper_key'],
+            "TRADE_API_SECRET":keys['paper_secret'],
+            "TRADE_API_ENDPOINT":keys['paper_endpoint']
+        }
+    stream_client = TradingStream(api_key=alpaca_keys["TRADE_API_KEY"], secret_key=alpaca_keys["TRADE_API_SECRET"], paper=is_paper)
+    stream_client.run()
 
 
 
 if __name__ == "__main__":
-    print(getAPIKey("/home/valkyrie/Desktop/trading bot/daytradingbotusingmachinelearning/apikey.txt"))
+    print(get_API_keys(""))
