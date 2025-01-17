0fc2a691ae315667517579b647c8bf5d3c023b46
Inventionist1 <isaacashofner@gmail.com>
2025-01-17 17:18:33 -0600

    added a function to pull clean training data

--- load_data.py
+++ load_data.py
@@ -1,9 +1,14 @@
 from dotenv import load_dotenv
 import os
+import pandas as pd
+import numpy as np
 
+#init of module
 load_dotenv()
+np.set_printoptions(precision=3, suppress=True)
 
-def get_api_keys():
+#function definitions
+def get_api_keys() -> dict:
     ALPACA_KEYS:list[str] = {
         "TRADE_API_ENDPOINT":os.getenv("ENDPOINT"),
         "TRADE_API_SECRET":os.getenv("SECRET"),
@@ -12,5 +17,19 @@ def get_api_keys():
     }
     return ALPACA_KEYS
 
+def get_training_data(file:str) -> list:
+    training_data = pd.read_csv(file,names = ["Date", 
+                                              "Close", 
+                                              "Volume", 
+                                              "Open", 
+                                              "High", 
+                                              "Low"])
+    training_data_features = training_data.copy()
+    training_data_features.pop("Date")
+    training_data_features = training_data_features.drop(index = 0, axis = 0)
+    training_data_features = np.array(training_data_features)
+    training_data_features = training_data_features.astype(np.float32)
+    return(training_data_features)
+
 if __name__ == "__main__":
-    pass
+    print(get_training_data("/home/valkyrie/Desktop/trading bot/daytradingbotusingmachinelearning/week_data.csv"))
