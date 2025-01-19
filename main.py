f72ec383360ebecf116e0094c61fa439e68ccac4
inventionist1 <42523031+inventionist1@users.noreply.github.com>
2025-01-19 15:40:19 -0600

    added an init function and made the main function run every second

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-import tensorflow as tf # type: ignore
+#import tensorflow as tf # type: ignore
 import load_data as data
+import time
+#needs pythoin 3.7 or above at minimum
+
+def init() -> None:
+    pass
 
 def main() -> None:
     pass
+    
 
 if __name__ == "__main__":
-    main()
+    init()
+    while True:
+        time0 = time.monotonic()
+        main()
+        time1 = time.monotonic()
+        if(1-(time1-time0) > 0):
+            time.sleep(1-(time1-time0))
+        else:
+            print("Can't keep up! running", int((1-(time1-time0))*-1000), "ms behind!")
+        
