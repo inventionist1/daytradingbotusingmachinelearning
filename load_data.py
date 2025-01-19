a57a399211f9c70f9d01568e307e6d2cbada5f3a
inventionist1 <42523031+inventionist1@users.noreply.github.com>
2025-01-19 14:23:52 -0600

    add bid/ask price get function

--- load_data.py
+++ load_data.py
@@ -2,18 +2,23 @@ from dotenv import load_dotenv
 import os
 import pandas as pd
 import numpy as np
+import requests
+import alpaca_trade_api as tradeapi
 
 #init of module
 load_dotenv()
 np.set_printoptions(precision=3, suppress=True)
 
 #function definitions
+def connect(LIVE_API_KEY, LIVE_SECRET_KEY, LIVE_BASE_URL):
+    live_api = tradeapi.REST(LIVE_API_KEY, LIVE_SECRET_KEY, LIVE_BASE_URL, api_version='v2')
+    return live_api
+
 def get_api_keys() -> dict:
     ALPACA_KEYS:list[str] = {
         "TRADE_API_ENDPOINT":os.getenv("ENDPOINT"),
         "TRADE_API_SECRET":os.getenv("SECRET"),
-        "TRADE_API_KEY":os.getenv("KEY"),
-        "PAPER_ACCOUNT":os.getenv("PAPER")
+        "TRADE_API_KEY":os.getenv("KEY")
     }
     return ALPACA_KEYS
 
@@ -31,5 +36,17 @@ def get_training_data(file:str) -> list:
     training_data_features = training_data_features.astype(np.float32)
     return(training_data_features)
 
+def get_bid_ask_price(symbol:str, live_api) -> float:
+    try:
+        quote = live_api.get_latest_quote(symbol.replace('-', '.'))
+        bid_price = quote.bp
+        ask_price = quote.ap
+    except Exception as e:
+        print("an error has occured")
+        print(e)
+        return None
+    return [bid_price, ask_price]
+
 if __name__ == "__main__":
-    print(get_training_data("/home/valkyrie/Desktop/trading bot/daytradingbotusingmachinelearning/week_data.csv"))
+    ALPACA_KEYS = get_api_keys();
+    print(get_bid_ask_price("TSLA",connect(ALPACA_KEYS["TRADE_API_KEY"],ALPACA_KEYS["TRADE_API_SECRET"],ALPACA_KEYS["TRADE_API_ENDPOINT"])))
