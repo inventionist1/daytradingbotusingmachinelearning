7e3ff6035392ef9d87872dedf77b83a28dff8785
inventionist1 <42523031+inventionist1@users.noreply.github.com>
2025-01-19 21:52:01 -0600

    added atr indicator current atr is used to get the current atr for the bar, past atr is the get the atr for the past 15m of candles

--- load_data.py
+++ load_data.py
@@ -2,8 +2,8 @@ from dotenv import load_dotenv
 import os
 import pandas as pd
 import numpy as np
-import requests
 import alpaca_trade_api as tradeapi
+from alpaca_trade_api.rest import TimeFrame
 
 #init of module
 load_dotenv()
@@ -35,18 +35,37 @@ def get_training_data(file:str) -> list:
 def get_bid_ask_price(symbol:str, live_api) -> float:
     try:
         quote = live_api.get_latest_quote(symbol.replace('-', '.'))
-        print(quote)
         bid_price = quote.bp
         ask_price = quote.ap
     except Exception as e:
         print("an error has occured")
         print(e)
         return None
-    return [bid_price, ask_price]
+    return (bid_price, ask_price)
 
-def calculate_average_true_range():
-    pass
+#math from https://www.investopedia.com/terms/a/atr.asp#toc-the-average-true-range-atr-formula
+def get_true_range(high:float, low:float, closing_price:float) -> float:
+    return max(
+        (high-low),
+        abs(high-closing_price),
+        abs(low-closing_price)
+    )
+
+def calculate_current_average_trading_range(past_atr:float, true_range:float, periods:int=14):
+    return ((past_atr*(periods-1)) + true_range)/periods
+
+def calculate_past_15m_average_trading_range(API, symbol:str):
+    cvs_bars_data = API.get_bars(symbol, TimeFrame.Hour, "2021-06-08", "2021-06-08", adjustment='raw', feed="IEX").df
+    clean_bars_data = np.array(cvs_bars_data)
+    clean_bars_data = clean_bars_data.astype(np.float64)
+    total:float = 0
+    #get average of the true ranges of each bar
+    for i in range(len(clean_bars_data)):
+        if i == 0:
+            continue
+        total += get_true_range(clean_bars_data[i][1],clean_bars_data[i][2],clean_bars_data[i-1][0])
+    atr:float = total/i
+    return atr
 
 if __name__ == "__main__":
-    ALPACA_KEYS = get_api_keys();
-    print(get_bid_ask_price("TSLA",))
+    pass
